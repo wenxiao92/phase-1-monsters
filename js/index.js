3fc60b7b93dd06ec7fbfd4ca4275823d34ae021f
Wen Xiao <wennstyle@gmail.com>
2021-11-04 23:19:17 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,84 +1,101 @@
 document.addEventListener("DOMContentLoaded", function() {
-//Test for button working
 
+//Function to increment/decrement the page. uses a callback function to fetchPage function
 function handlePage(){
     let backBtn = document.querySelector('#back')
     let forwardBtn = document.querySelector('#forward')    
     let pageNumber =1
 
-    forwardBtn.addEventListener('click', function(e){
-        e.preventDefault()
+    forwardBtn.addEventListener('click', function(){
         pageNumber = pageNumber +1
-        console.log(pageNumber)
+        document.querySelector('#monster-container').innerHTML = ''
+        //console.log(pageNumber)
+        fetchPage(pageNumber)
     })
 
-    backBtn.addEventListener('click', function(e){
-        e.preventDefault()
+    backBtn.addEventListener('click', function(){
         if(pageNumber === 1) {
             return;
         } else {
             pageNumber = pageNumber -1
-            console.log(pageNumber)
+            document.querySelector('#monster-container').innerHTML = ''
+            //console.log(pageNumber)
+            fetchPage(pageNumber)
         }
     })
     //console.log(pageNumber)
 }
 
-// function fetchPage(currentPage){
-//     fetch(`http://localhost:3000/monsters/?_limit=50&_page${currentPage}`)
-//     .then(resp => resp.json())
-//     .then(data => console.log(data))
-// }
-
-handlePage()
-
-// fetchData(handlePage)
-
-    // fetch("http://localhost:3000/monsters")
-    // .then(resp => resp.json())
-    // .then(function(data) {
-
-    //     function findProp(obj, prop) {
-    //         let result = [];
-    //         function recursivelyFindProp(o, keyName) {
-    //           Object.keys(o).forEach(function(key) {
-    //             if(typeof o[key] === 'object') {
-    //               recursivelyFindProp(o[key], keyName);
-    //             } else {
-    //               if (key === keyName) result.push(o[key]);
-    //             }
-    //           });
-    //         }
-    //         recursivelyFindProp(obj, prop);
-    //         return result;
-    //       }
-    //       let monsterNames = findProp(data, "name")
-    //       let monsterAges = findProp(data, "age")
-    //       let monsterDesc = findProp(data, "description")
+function fetchPage(currentPage=1){
+    fetch(`http://localhost:3000/monsters/?_limit=50&_page=${currentPage}`)
+    .then(resp => resp.json())
+    .then(function(data){
+        function findProp(obj, prop) {
+            let result = [];
+            function recursivelyFindProp(o, keyName) {
+              Object.keys(o).forEach(function(key) {
+                if(typeof o[key] === 'object') {
+                  recursivelyFindProp(o[key], keyName);
+                } else {
+                  if (key === keyName) result.push(o[key]);
+                }
+              });
+            }
+            recursivelyFindProp(obj, prop);
+            return result;
+          }
+          let monsterNames = findProp(data, "name")
+          let monsterAges = findProp(data, "age")
+          let monsterDesc = findProp(data, "description")
           //console.log(monsterNames, monsterAges, monsterDesc) //working
 
-          //Test for output is in increments of 50
-        //   for(j=1; j<Math.ceil(data.length/50)+1; j++) {
-        //       for(i = 0; ((i*1)+(j*50)-1) <50*j; i++) {
-        //           console.log("page ",j)
-        //         for(k=0; k<50; k++) {
-        //             m = (j-1)*50+k
-        //             if(m===data.length){
-        //                 return;
-        //             } else{
-        //                 console.log(m)
-        //             }
-        //         }
-        //       }
-        //   }
+          for(i=0; i<data.length; i++) {
+              createDisplayInfo(monsterNames[i], monsterAges[i], monsterDesc[i])
+          }
 
-    //})// end of second .then
+    })
+}
 
+//function that gets used to display one monster's information (this gets called back within fetchPage function)
+function createDisplayInfo(name, age, description) {
+    let div = document.createElement('div')
+    div.innerHTML = `
+    <h2>${name} </h2>
+    <h4>Age: ${age} </h4>
+    <p>Bio: ${description}</p>     
+    `
+    document.querySelector('#monster-container').append(div)
+}
 
+fetchPage() //render initial 50 monsters to the page
+handlePage() //enables the button click
 
+//create the form so that we can add monster
+let form = document.createElement('form')
+form.setAttribute("id","monster-form")
+form.innerHTML = 
+`<input id='name' placeholder='name...'>
+ <input id='age' placeholder='age...'>
+ <input id='description' placeholder='description...'>
+ <button>Create</button>`
 
+document.querySelector('#create-monster').append(form)
 
+addEventListener('submit',function(e){
+  e.preventDefault()
+  fetch("http://localhost:3000/monsters",{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({
+      name: document.querySelector("#name").value,
+      age: document.querySelector("#age").value,
+      description: document.querySelector("#description").value
+    })
+  })
 
+})
 
 
 }) //end of DOM Loads
